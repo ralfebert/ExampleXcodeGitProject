5921bd1a57d9040ebfc58eb204fde47399649ef2
Ralf Ebert <info@ralfebert.de>
2020-06-23 16:23:19 +0200

    Initial Commit

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by Ralf Ebert on 23.06.20.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloApp/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  HelloApp
+//
+//  Created by Ralf Ebert on 23.06.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
