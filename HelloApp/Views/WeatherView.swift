a390d0fa7b7379402e973504c58bf2c900392a99
Ralf Ebert <info@ralfebert.de>
2020-06-23 16:26:47 +0200

    prepared Weather View

---
+++ HelloApp/Views/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  HelloApp
+//
+//  Created by Ralf Ebert on 23.06.20.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("The Weather")
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
